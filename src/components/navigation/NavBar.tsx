6914dea920d729a4d33d0802ec9843f6eccca767
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-21 19:05:25 +0600

    Add big screen navigation

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import UseMediaQuery from "../../hooks/useMediaQuery";
+import { navData } from "./NavData";
+import NavLink from "./NavLink";
 
 interface propTypes {
   selectedPage: any;
@@ -14,6 +16,17 @@ function NavBar({ selectedPage, setSelectedPage }: propTypes) {
       <nav className={`z-40 w-full fixed top-0 py-6`}>
           <div className="flex items-center justify-between mx-auto w-5/6">
               <h4 className="font-playfair text-3xl font-bold">JE</h4>
+              {isAboveSmallScreens ? (
+                <div className="flex justify-between gap-16 font-opensans text-md font-semibold">
+                  {navData.map((page) => (
+                    <NavLink key={page} page={page} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                  ))}
+                </div>
+              ) : (
+                <div>
+
+                </div>
+              )}
           </div>
       </nav>
   )
