28d426b9a1f091f3dec9138e3c4199d468f0826a
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-23 21:03:39 +0600

    Add profile image

--- src/components/home/LandingSection.tsx
+++ src/components/home/LandingSection.tsx
@@ -15,13 +15,11 @@ function LandingSection({ setSelectedPage }: propTypes) {
     <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
         {isAboveMediaScreen ? (
-          <div className="relative z-0 ml-20 before:absolute before:top-20 before:left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
-            <img src={ProfileImage} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" alt="" />
+          <div className="relative z-0 ml-20 before:absolute before:-top-16 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+            <img src={ProfileImage} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[400px]" alt="" />
           </div>
         ) : (
-          <div>
-
-          </div>
+          <img src={ProfileImage} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[400px]" alt="" />
         )}
       </div>
     </section>
