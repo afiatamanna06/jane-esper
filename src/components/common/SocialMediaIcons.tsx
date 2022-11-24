50afc35508ac5e30d4d89428dc8b70f2062d9274
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-24 10:59:30 +0600

    Add social media icons mapping

--- src/components/common/SocialMediaIcons.tsx
+++ src/components/common/SocialMediaIcons.tsx
@@ -1,13 +1,18 @@
-
+import { socialLinks } from "./SocialMediaData";
+import { Link } from "react-router-dom";
 
 function SocialMediaIcons() {
   return (
     <div className="flex justify-center md:justify-start items-center my-10 gap-7">
-        <a className="hover:opacity-50 transition duration-500">
-
-        </a>
+      {socialLinks.map(({ icon, link }) => (
+        <Link to={link} target="_blank" rel="noreferrer">
+          <div className="hover:opacity-50 transition duration-500">
+              {icon}
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
-export default SocialMediaIcons
+export default SocialMediaIcons;
