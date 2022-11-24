ef4193d6eb381c011ae8e70da00876fafa688ab5
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-24 23:44:18 +0600

    Add my skills image

--- src/components/skills/MySkills.tsx
+++ src/components/skills/MySkills.tsx
@@ -1,9 +1,9 @@
-import UseMediaQuery from "../../hooks/useMediaQuery"
-import LineGradient from "../common/LineGradient"
-import { motion } from "framer-motion"
+import UseMediaQuery from "../../hooks/useMediaQuery";
+import LineGradient from "../common/LineGradient";
+import { motion } from "framer-motion";
+import SkillsImage from "../../assets/skills-image.png"
 
 function MySkills() {
-
   const isAboveMediaScreen = UseMediaQuery("(min-width: 1060px)");
 
   return (
@@ -18,18 +18,34 @@ function MySkills() {
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
-          }}>
-            <p className="font-playfair font-semibold text-4xl mb-5">
-              MY <span className="text-red">SKILLS</span>
-            </p>
-            <LineGradient width="w-1/3" />
-            <p className="mt-10 mb-7">
-              Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut fames odio in at.
-            </p>
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+            fames odio in at.
+          </p>
         </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveMediaScreen ? (
+            <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]">
+              <img
+                src={SkillsImage}
+                className="z-10 w-full"
+                alt=""
+              />
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default MySkills
+export default MySkills;
