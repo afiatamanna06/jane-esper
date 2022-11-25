5518cf6d7c00435a31c391b76529ca751cc4005d
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-25 10:42:14 +0600

    Fix social link problems

--- src/components/common/SocialMediaData.tsx
+++ src/components/common/SocialMediaData.tsx
@@ -3,18 +3,18 @@ import { FiFacebook, FiInstagram, FiTwitter, FiLinkedin } from "react-icons/fi";
 export const socialLinks = [
   {
     icon: <FiLinkedin size={26} />,
-    link: "https://www.linkedin.com/",
+    link: "https://www.linkedin.com",
   },
   {
     icon: <FiTwitter size={26} />,
-    link: "https://www.twitter.com/",
+    link: "https://www.twitter.com",
   },
   {
     icon: <FiFacebook size={26} />,
-    link: "https://www.facebook.com/",
+    link: "https://www.facebook.com",
   },
   {
     icon: <FiInstagram size={26} />,
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com",
   },
 ];
--- src/components/common/SocialMediaIcons.tsx
+++ src/components/common/SocialMediaIcons.tsx
@@ -1,15 +1,15 @@
 import { socialLinks } from "./SocialMediaData";
-import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function SocialMediaIcons() {
   return (
     <div className="flex justify-center md:justify-start items-center my-10 gap-5">
       {socialLinks.map(({ icon, link }) => (
-        <Link to={link} target="_blank" rel="noreferrer">
+        <a href={link} target="_blank" rel="noopener">
           <div className="hover:opacity-50 transition duration-500">
               {icon}
           </div>
-        </Link>
+        </a>
       ))}
     </div>
   );
