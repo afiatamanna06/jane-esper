26adffce24c54a26306c06201ab8e2d59db2cd11
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-26 22:53:55 +0600

    Add contact section image

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,9 +1,72 @@
-
+import LineGradient from "../common/LineGradient";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactImage from "../../assets/contact-image.jpeg";
 
 function Contact() {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <section id="contact" className="py-48">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+          </p>
+          <div className="flex md:justify-end my-5">
+            <LineGradient width="mx-auto w-2/4" />
+          </div>
+        </div>
+      </motion.div>
+      <div className="md:flex md:justify-between gap-16 mt-5">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: -50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <img src={ContactImage} alt="" />
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: -50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          
+        </motion.div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
