4e72f22e21f91ebe73c7ec8964330271a954cd4d
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-27 11:07:05 +0600

    Update footer file

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,16 @@
-
+import SocialMediaIcons from "../common/SocialMediaIcons"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-red pt-8 pb-16">
+        <div className="w-5/6 mx-auto">
+            <SocialMediaIcons />
+            <div className="md:flex justify-center md:justify-between text-center">
+                <p className="font-playfair font-semibold text-xl text-yellow">JANE ESPER</p>
+                <p className="font-playfair text-md text-yellow mt-2 md:mt-0">©2022 ESPER. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
   )
 }
 
